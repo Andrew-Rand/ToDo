e2955eea14035022625b6ec2b9dae0da40afb1ee
toxi <xseven693@gmail.com>
2021-07-27 15:03:04 +0300

    adde templates with style (bootstap)

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello')
 def hello():
-    return "hello"
+    return render_template('index.html')
 
 
+@app.route('/')
 @app.route('/main')
 def main():
-    return 'main page'
+    return render_template('main.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/user/<string:username>/<int:id>')  # Обработка любых username и id
+def user(username, id):
+    return f"user page: {username}, id: {id}"
 
 
 if __name__ == "__main__":
